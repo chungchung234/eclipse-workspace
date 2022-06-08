a3a648027c8803d8998cf2bd1f03334c19d7591c
이충현 <a8207637@naver.com>
2022-06-08 12:46:33 +0900

    1

--- untitled/src/test/Cal.java
+++ untitled/src/test/Cal.java
@@ -6,10 +6,23 @@ public class Cal {
     public static void main(String[] args) {
         Calendar cal = Calendar.getInstance();
 
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH) + 1; // 0~11 로 계산 따라서 +1 필수
-        int day = cal.get(Calendar.DATE);
-6
+        int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        cal.set(Calendar.DAY_OF_MONTH , 1);
+        int firstWeek = cal.get(Calendar.DAY_OF_WEEK);
+        for (int j = 0; j < firstWeek; j++) {
+            System.out.print(
+                    "*"
+            );
+
+        }
+        for (int i = 1; i < lastDay+1; i++) {
+            System.out.print(i);
+            if((i+firstWeek)%7==0){
+                System.out.println();
+            }
+        }
+
         System.out.println();
 
     }
