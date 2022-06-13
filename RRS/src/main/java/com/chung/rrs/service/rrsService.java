c8b2f6f49a43a3d9a58a596314bdae42b55b0ddc
이충현 <a8207637@naver.com>
2022-06-13 18:33:15 +0900

    6/13

--- RRS/src/main/java/com/chung/rrs/service/rrsService.java
+++ RRS/src/main/java/com/chung/rrs/service/rrsService.java
@@ -1,14 +1,30 @@
 package com.chung.rrs.service;
 
 public interface rrsService {
+	/**
+	 *
+	 */
 	public static void add() {}
+	
+	/**
+	 * 
+	 */
 	public static void delete() {}
+	/**
+	 * 
+	 */
 	public static void modify() {}
-	public static void Pick_me_one() {}
+	/**
+	 * 
+	 */
 	public static void search() {}
+	/**
+	 * 
+	 */
 	public static void all_data() {}
-	private boolean check_Format() {
-		return false;
-	}
+	/**
+	 * 
+	 */
+	public static void pick_Me_One() {}
 	
 }
